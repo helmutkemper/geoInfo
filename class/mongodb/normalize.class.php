030ff05227bbb87ca09ea9fbeedbe2feda4006e9
kemper <helmut.kemper@gmail.com>
2016-05-01 08:40:15 -0300

    comentário do código

--- class/mongodb/normalize.class.php
+++ class/mongodb/normalize.class.php
@@ -7,37 +7,84 @@
     protected $appendFirstCArr;
     protected $appendLastCArr;
 
-    function __construct()
+    public function __construct()
     {
+      parent::__construct();
       $this->garbageCollectorCArr = null;
     }
 
-    function setOutputJSonMobile(){
+    /** Define a saída em formato json com cabeçalho complementar para paginação. [ padrão ]
+     * @code
+      {
+        "meta":
+        {
+          "limit":unsigned int,
+          "next":"string URL or null",
+          "offset":unsigned int,
+          "previous":string URL or null,
+          "total_count":unsigned int,
+          "success":boolean,
+          "action":array of string,
+          "error":array of string
+        },
+        "objects":
+          array de objetos.
+      }
+     * @endcode
+     */
+    protected function setOutputJSonMobile(){
+      // Arquiva o formato de saída.
       global $outputTypeGStr;
 
       $outputTypeGStr = "json_mobile";
     }
 
-    function setOutputJSon(){
+    /** Define a saída como um simples json, sem formatação de dados.
+     * Este formato é muito útil para se trabalhar com classes javascript de terceiros. Perceba porém que não são
+     * passados dados complementares.
+     */
+    protected function setOutputJSon(){
+      // Arquiva o formato de saída.
       global $outputTypeGStr;
 
       $outputTypeGStr = "json";
     }
 
-    function setOutputCsv(){
+    /** Define a saída como sendo um arquivo separado por vírgula, um formato mais fácil de ser importado para alguns
+     * dispositivos IoT.
+     */
+    protected function setOutputCsv(){
+      // Arquiva o formato de saída.
       global $outputTypeGStr;
 
       $outputTypeGStr = "csv";
     }
 
-    function MongoFindToOutput( $toFindAArr ){
+    /** Executa uma query e compatibiliza a saída com o framework.
+     *
+     * @param $toFindAArr array com a query do MongoDB
+     * @return array no formato especificado.
+     * @see setOutputJSonMobile
+     * @see setOutputJSon
+     * @see setOutputCsv
+     */
+    protected function MongoFindToOutput( $toFindAArr ){
+      // Limite passado ao MongoDB
       global $pageLimitGUInt;
+
+      // Offset passado ao MongoDB
       global $pageOffsetGUInt;
+
+      // Total de dados encontrados
       global $pageTotalGObj;
+
+      // Query anterior passada via query string, criptografada.
       global $pagePreviousQueryGStr;
 
+      // Prepara a query atual para ser criptografada
       $pagePreviousQueryGStr = $toFindAArr;
 
+      // Testa se há uma query anterior
       $passLBoo = false;
 
       if( isset( $_REQUEST[ "query" ] ) ){
@@ -57,6 +104,7 @@
         }
       }
 
+      // Caso não haja, monta a query atual
       if( $passLBoo == false ) {
         $cursorLObj = $this->collectionCObj->find( $toFindAArr );
         $pageTotalGObj = $cursorLObj->count();
@@ -64,9 +112,11 @@
         $cursorLObj->limit( $pageLimitGUInt );
       }
 
+      // Monta o dado a ser enviado para a saída.
       $returnLArr = array();
       foreach( $cursorLObj as $lineLArr ){
 
+        // Verifica se há dados a serem recuperados
         if( is_array( $this->rescueCollectionCArr ) ){
           $returnLineLArr = array();
           foreach( $this->rescueCollectionCArr as $rescueDataLStr ){
@@ -80,8 +130,10 @@
           }
           $returnLArr[] = $returnLineLArr;
         }
+        // Caso não haja dados a serem recuperados,monta o dado original.
         else
         {
+          // Procura por dados a serem excluídos.
           if( is_array( $this->garbageCollectorCArr ) )
           {
             foreach( $this->garbageCollectorCArr as $garbageLStr )
@@ -93,15 +145,17 @@
           {
             util::garbageCollector( $this->garbageCollectorCArr, $lineLArr );
           }
-
           $returnLArr[] = $lineLArr;
         }
       }
 
+      // Verifica se há alguma informação determinada pelo usuário para ser colocada na primeira linha da saída.
+      // Isto é muito útil para exportar CSV e montar planilhas.
       if( is_array( $this->appendFirstCArr ) ){
         $returnLArr = array_merge( array( $this->appendFirstCArr ), $returnLArr );
       }
 
+      // Verifica se há alguma informação determinada pelo usuário para ser colocada na última linha da saída.
       if( is_array( $this->appendLastCArr ) ){
         $returnLArr = array_merge( $returnLArr, array( $this->appendLastCArr ) );
       }
@@ -109,19 +163,48 @@
       return $returnLArr;
     }
 
-    public function setRescueCollection( $collectorAArr ){
+    /** Determina quais chaves vão ser recuperados na query atual.
+     * Esta função se sobrepõe as demais e é usada para recuperar chaves específicas em uma coleção de dados.
+     * A principal funcionalidade dela é limpar os dados, principalmente para arquivos CSV.
+     * @code
+        // dado: { location:[-8.1313,-34.9146], key:{ subKey1:{ subKey2: { subKeyN: value } } } }
+        $this->setRescueCollection( array( "location.0", "location.1", "key.subKey1.subKey2.subKeyN" ) );
+     * @endcode
+     * @param $collectorAArr [array|string] Com as chaves desejadas separadas por ponto.
+     * @see setGarbageCollector
+     */
+    protected function setRescueCollection( $collectorAArr ){
       $this->rescueCollectionCArr = $collectorAArr;
     }
 
-    public function setGarbageCollector( $collectorAArr ){
+    /** Determina quais chaves vão ser excluídas da saída.
+     * Esta função é útil para apagar dados críticos, como senhas do usuário.
+     * @code
+        $this->setGarbageCollector( array( "id_node", "id_changesets", "id_user", "version", "visible", , "key.subKey1.subKey2.subKeyN" ) );
+     * @endcode
+     * @param $collectorAArr [array|string] Com as chaves desejadas separadas por ponto.
+     * @see setRescueCollection
+     */
+    protected function setGarbageCollector( $collectorAArr ){
       $this->garbageCollectorCArr = $collectorAArr;
     }
 
-    public function setAppendFirst( $appendAArr ){
+    /** Adiciona o valor contido no array a primeira linha da saída.
+     * @code
+        $this->setAppendFirst( array( "lat", "lgn", "maxSpeed" ) );
+     * @endcode
+     * @param $appendAArr array de string
+     * @see setAppendLast
+     */
+    protected function setAppendFirst( $appendAArr ){
       $this->appendFirstCArr = $appendAArr;
     }
 
-    public function setAppendLast( $appendAArr ){
+    /** Adiciona o valor contido no array a última linha da saída.
+     * @param $appendAArr array de string
+     * @see setAppendFirst
+     */
+    protected function setAppendLast( $appendAArr ){
       $this->appendLastCArr = $appendAArr;
     }
   }
--- userClass/nodes/nodes.class.php
+++ userClass/nodes/nodes.class.php
@@ -1,5 +1,10 @@
 <?php
 
+  /** Esta é uma classe de exemplo de como usar o framework.
+   * Para usar este exemplo, depois de instalado, use os endereço
+   * http://dominio.com/nodes/nodes/getMobileOutput/amenity:school/-8.1313/-34.9146/-8.1047/-34.8710
+   * http://dominio.com/folder/class/method/type:value;type:value;type:value/bottom/left/upper/right
+   */
   class nodes extends normalize
   {
     function __construct()
@@ -8,42 +13,72 @@
       self::connect();
     }
 
+    //SET @orig_lat=-8.116137;
+    //SET @orig_lon=-34.897488;
+    //SET @dist=1000;
+    //SELECT *,(((acos(sin((@orig_lat*pi()/180)) * sin((dest.latitude*pi()/180))+cos((@orig_lat*pi()/180))*cos((dest.latitude*pi()/180))*cos(((@orig_lon-dest.longitude)*pi()/180))))*180/pi())*60*1.1515*1609.344) as distance FROM nodes AS dest HAVING distance < @dist ORDER BY distance ASC LIMIT 100;
+
+    /** Define quais métodos são de acesso externo pelo servidor.
+     *
+     * @return array de strinf com os nomes de métodos públicos
+     */
     public function getNamesOfPublicMethods(){
       return array(
-        "get"
+        "getMobileOutput",
+        "info"
       );
     }
 
-    //http://localhost/nodes/nodes/get/amenity:school/-8.1313/-34.9146/-8.1047/-34.8710
-    //http://localhost/nodes/nodes/get/type:value/bottom/left/upper/right
-    public function get(){
+    /** Faz uma consulta por pontos de interesse dentro de uma área retangular.
+     * http://dominio.com/nodes/nodes/getMobileOutput/amenity:school/-8.1313/-34.9146/-8.1047/-34.8710
+     * http://dominio.com/folder/class/method/type:value;type:value;type:value/bottom/left/upper/right
+     * @return array com os dados a serem passados à saída.
+     */
+    public function getMobileOutput(){
+      // Recebe todos os parâmetros passados por URL
       global $parameterGArr;
+
+      // Define o formato de saída.
       $this->setOutputJSonMobile();
+
+      // Define o nome da coleção do MongoDB
       $this->setCollection( "nodes" );
+
+      // Define as chaves desnecessárias
       $this->setGarbageCollector( array( "tags.highway", "_id", "id_node", "id_changesets", "id_user", "version", "visible" ) );
       //$this->setAppendFirst( array( "lat", "lgn", "maxSpeed" ) );
       //$this->setRescueCollection( array( "location.0", "location.1", "tags.maxspeed.val" ) );
 
       $filterLArr = array();
 
+      // Explode o primeiro parâmetro, type:value
       if( strlen( $parameterGArr[ 0 ] ) > 0 ){
+        // Procura pelo ';', usado como separador de tipo.
         $parameterGArr[ 0 ] = explode( ";", $parameterGArr[ 0 ] );
 
+        // Para cada tipo, separa o tipo em chave:valor
         foreach( $parameterGArr[ 0 ] as $filterDataLStr ){
+          // Separa chave:valor
           $filterDataLArr = explode( ":", $filterDataLStr );
-
+          // O MongoDB necessita que valores numéricos sejam realmente numéricos, e não string numéricas
           if( is_numeric( $filterDataLArr[ 1 ] ) )
           {
+            // Monta o tipo int
             if( $filterDataLArr[ 1 ] == ( int )$filterDataLArr[ 1 ] )
             {
               $filterDataLArr[ 1 ] = ( int )$filterDataLArr[ 1 ];
             }
+            // Monta o tipo double/float
             else if( $filterDataLArr[ 1 ] == ( double )$filterDataLArr[ 1 ] )
             {
               $filterDataLArr[ 1 ] = ( double )$filterDataLArr[ 1 ];
             }
           }
 
+          // Veja a documentação de como o banco foi montado.
+          // As chaves são formadas por tags => key1 => key2 => keyN => val: valor
+
+          // Monta um $or caso a chave já exista
           if( isset( $filterLArr[ "tags.{$filterDataLArr[ 0 ]}.val" ] ) ){
             $filterLArr[ "tags.{$filterDataLArr[ 0 ]}.val" ][ '$in' ] = array_merge( $filterLArr[ "tags.{$filterDataLArr[ 0 ]}.val" ][ '$in' ], array( $filterDataLArr[ 1 ] ) );
           }
@@ -52,6 +87,7 @@
           }
         }
 
+        // Monta o array da busca de tags.
         $orLArr = array();
         foreach( $filterLArr as $filterKeyLStr => $filterValueLArr ){
           $orLArr[] = array( $filterKeyLStr => $filterValueLArr );
@@ -60,7 +96,7 @@
         $filterLArr = array( '$or' => $orLArr );
       }
 
-
+      // Monta a query $geoWithin.$box para limitar a busca
       if( ( is_numeric( $parameterGArr[ 1 ] ) ) && ( is_numeric( $parameterGArr[ 2 ] ) ) && ( is_numeric( $parameterGArr[ 3 ] ) ) && ( is_numeric( $parameterGArr[ 4 ] ) ) ){
         $filterLArr = array_merge(
           $filterLArr,
